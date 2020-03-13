5979994365269ec8ae72bf17e317a88ccaa7b8a8
Alex Ricci <49104655+AxlRicci@users.noreply.github.com>
2020-03-13 15:38:05 -0400

    work on schoolProfile component

--- src/components/SchoolProfile.js
+++ src/components/SchoolProfile.js
@@ -2,11 +2,28 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class SchoolProfile extends Component {
+  constructor() {
+    super();
+  }
+
+  componentDidMount() {
+    this.defineSchool()
+  }
+  
+  defineSchool() {
+    this.props.state == null ? 
+      (console.log("state is unrendered"))
+    : (
+      this.setState(this.props.state)
+    )
+  }
+
   render() {
+    console.log(this.state);
     return (
       null
-    );
-  }
+      )
+    }
 }
 
 const mapStateToProps = function(state) {
